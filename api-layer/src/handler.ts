0904cf1d909e400e04c85345a39591a82ba59c6d
Rama Varma Vinay Varma [SchoolBell.chat] <rama.varma@schoolbell.chat>
2021-12-12 17:49:54 +1300

    non working layer, but working deployment

--- api-layer/src/handler.ts
+++ api-layer/src/handler.ts
@@ -1,12 +1,14 @@
 import { APIGatewayEvent, Callback, Context, SNSEvent, SNSMessage } from 'aws-lambda';
 
+import {TestClass} from '@demo/layer-demo/libs/test-class'
+const testClass: TestClass = new TestClass();
 export const helloWorld = ((req: APIGatewayEvent, context: Context, callback: Callback) => {
 
     const response = {
 		statusCode: 200,
 		body: JSON.stringify({
 			secretValue: context.awsRequestId,
-			message: 'Hello world!!!'
+			message: testClass.getGreeting()
 		})
 	};
 
